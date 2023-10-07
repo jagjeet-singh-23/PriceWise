fc00999b34bf66d1a90447cb9c755c8fe3f33e41
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-10-07 19:53:44 +0530

    modified:   lib/actions/index.ts 	new file:   lib/models/product.model.ts

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,7 +1,9 @@
 "use server";
-
+import { revalidatePath } from "next/cache";
+import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scrapper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) {
@@ -13,6 +15,30 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     if (!scrapedProduct) {
       return;
     }
+    let product = scrapedProduct;
+    const existingProduct = await Product.findOne({ url: product.url });
+
+    if (existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      };
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: product.url },
+      product,
+      { upsert: true, new: true }
+    );
+    revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
     throw new Error(`Error scraping product: ${error.message}`);
   }
