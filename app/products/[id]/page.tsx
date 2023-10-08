07638aba8372b4d13221e8b30115f16ecea7bcab
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-10-08 18:27:28 +0530

    modified:   app/products/[id]/page.tsx

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,8 @@
 import { getProductById } from "@/lib/actions";
+import { formatNumber } from "@/lib/utils";
+import { Product } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 type Props = {
@@ -7,7 +10,7 @@ type Props = {
 };
 
 const ProductDetails = async ({ params: { id } }: Props) => {
-  const product = await getProductById(id);
+  const product: Product = await getProductById(id);
   if (!product) {
     redirect("/");
   }
@@ -22,6 +25,90 @@ const ProductDetails = async ({ params: { id } }: Props) => {
             height={400}
             className="mx-auto"
           />
+        </div>
+        <div className="flex-1 flex-col">
+          <div className="flex justify-between items-start gap-5 flex-wrap pb-6">
+            <div className="flex flex-col gap-3">
+              <p className="text-[28px] text-secondary font-semibold">
+                {product.title}
+              </p>
+              <Link
+                href={product.url}
+                target="_blank"
+                className="text-base text-black opacity-50"
+              >
+                Visit Product
+              </Link>
+            </div>
+            <div className="flex items-center gap-3">
+              <div className="product-hearts">
+                <Image
+                  src={"/assets/icons/red-heart.svg"}
+                  alt="heart"
+                  width={20}
+                  height={20}
+                />
+                <p className="text-base font-semibold text-[#D46F77]">
+                  {product.reviewsCount}
+                </p>
+              </div>
+              <div className="p-2 bg-white-200 rounded-10">
+                <Image
+                  src={"/assets/icons/bookmark.svg"}
+                  alt="bookmark"
+                  width={20}
+                  height={20}
+                />
+              </div>
+              <div className="p-2 bg-white-200 rounded-10">
+                <Image
+                  src={"/assets/icons/share.svg"}
+                  alt="share"
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="product-info">
+            <div className="flex flex-col gap-2">
+              <p className="text-[34px] font-bold text-secondary">
+                {product.currency}
+                {formatNumber(product.currentPrice)}
+              </p>
+              <p className="text-[21px] text-black opacity-50 line-through">
+                {product.currency}
+                {formatNumber(product.originalPrice)}
+              </p>
+            </div>
+            <div className="flex flex-col gap-4">
+              <div className="flex gap-3">
+                <div className="product-stars">
+                  <Image
+                    src={"/assets/icons/star.svg"}
+                    alt="star"
+                    width={16}
+                    height={16}
+                  />
+                  <p className="text-sm text-primary-orange font-semibold">
+                    {product.stars || "25"}
+                  </p>
+                </div>
+                <div className="product-reviews">
+                  <Image 
+                    src={"/assets/icons/comment.svg"}
+                    alt="comment"
+                    width={16}
+                    height={16}
+                  />
+                  <p className="text-sm text-black opacity-50">
+                    {product.reviewsCount} reviews
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
