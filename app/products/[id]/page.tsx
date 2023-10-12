fae8ba40ed98ba44c509d47dfcc029ec4e439352
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-10-12 19:55:49 +0530

    modified:   app/products/[id]/page.tsx 	modified:   lib/actions/index.ts

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,6 @@
 import PriceInfoCard from "@/components/PriceInfoCard";
-import { getProductById } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
+import { getProductById, getSimilarProducts } from "@/lib/actions";
 import { formatNumber } from "@/lib/utils";
 import { Product } from "@/types";
 import Image from "next/image";
@@ -15,6 +16,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
   if (!product) {
     redirect("/");
   }
+  const similarProducts = await getSimilarProducts(id);
   return (
     <div className="product-container">
       <div className="flex gap-28 xl:flex-row flex-col">
@@ -148,7 +150,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
           Modal
         </div>
       </div>
-      <div className="flex flex-col gap-16 border-2 border-red-500">
+      <div className="flex flex-col gap-16">
         <div className="flex flex-col gap-5">
           <h3 className="text-2xl text-secondary font-semibold">
             Product Description
@@ -179,6 +181,16 @@ const ProductDetails = async ({ params: { id } }: Props) => {
           </Link>
         </button>
       </div>
+      {similarProducts && similarProducts.length > 0 && (
+        <div className="py-16 flex flex-col w-full gap-2">
+          <p className="section-text">Similar Products</p>
+          <div className="flex flex-wrap gap-10 mt-7 w-full">
+            {similarProducts.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
