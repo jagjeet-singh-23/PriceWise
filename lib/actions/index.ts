49e72669e7a2f7482d7bfa09d2b971564a3260ab
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-10-21 15:29:02 +0530

    modified:   app/products/[id]/page.tsx 	new file:   components/Modal.tsx 	modified:   lib/actions/index.ts 	new file:   lib/nodemailer/index.ts 	modified:   package-lock.json 	modified:   package.json

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -4,6 +4,8 @@ import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scrapper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) {
@@ -80,6 +82,28 @@ export async function getSimilarProducts(productId: string) {
       _id: { $ne: productId },
     }).limit(3);
     return similarProducts;
+  } catch (error: any) {
+    console.log(error);
+  }
+}
+export async function addUserEmailToProduct(
+  productId: string,
+  userEmail: string
+) {
+  try {
+    const product = await Product.findById(productId);
+    if (!product) {
+      return;
+    }
+    const userExists = await product.users.some(
+      (user: User) => user.email === userEmail
+    );
+    if (!userExists) {
+      product.users.push({ email: userEmail });
+      await product.save();
+      const emailContent = await generateEmailBody(product, "WELCOME");
+      await sendEmail(emailContent, [userEmail]);
+    }
   } catch (error: any) {
     console.log(error);
   }
